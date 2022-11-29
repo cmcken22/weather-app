077c44cfbad5abf715e95abc1ef23c92afdcec92
test <test@tests-MacBook-Pro.local>
2022-11-29 01:09:37 -0500

    fix: triggering build

--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,6 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
-      // {
-      //   test: /(\.css|.scss)$/,
-      //   use: [{
-      //     loader: "style-loader" // creates style nodes from JS strings
-      //   }, {
-      //     loader: "css-loader" // translates CSS into CommonJS
-      //   }]
-      // }
       {
         test: /\.less$/i,
         use: [
